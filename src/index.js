2f7d9e378e6bd7ba47847ee48e49d9384a477314
AlisonGraham75 <alison.graham10@gmail.com>
2020-08-03 16:22:15 -0600

    Add ability to send geolocation

--- src/index.js
+++ src/index.js
@@ -29,6 +29,10 @@ io.on('connection', (socket) => {
         io.emit('message', message)
     })
 
+    socket.on('sendLocation', (coords) => {
+        io.emit('message', 'https://google.com/maps?q=' + coords.latitude + ',' +  coords.longitude)
+    })
+
     //send message to all remaining clients on disconnect
     socket.on('disconnect', () => {
         io.emit('message', 'A user has left.')
