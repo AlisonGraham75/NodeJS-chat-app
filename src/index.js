adaef8f87940f793d97c5e471c36940a97c50f0f
AlisonGraham75 <alison.graham10@gmail.com>
2020-08-04 11:18:54 -0600

    Added p;rofanity filter and location acknologment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 
 const app = express()
@@ -24,13 +25,19 @@ io.on('connection', (socket) => {
     //send to every client except the current
     socket.broadcast.emit('message', "A new user has joined")
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+        if (filter.isProfane(message)){
+            return callback('Profanity is not allowed!')
+        }
         //emits to all connected clients
         io.emit('message', message)
+        callback()
     })
 
-    socket.on('sendLocation', (coords) => {
+    socket.on('sendLocation', (coords, callback) => {
         io.emit('message', 'https://google.com/maps?q=' + coords.latitude + ',' +  coords.longitude)
+        callback()
     })
 
     //send message to all remaining clients on disconnect
